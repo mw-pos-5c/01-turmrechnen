9d8f1f12208beda03d57449ea90f7adfa03abbe5
Michael Wiesinger <michw2014@gmail.com>
2021-09-15 10:06:01 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'turmrechner';
+  startValue = 0;
+  height = 0;
+
+  result: string[] = [];
+
+
+  calc(): void {
+
+    let value = this.startValue;
+
+    for (let i = 0; i <= this.height; i++) {
+      const result = value * (i+2);
+      this.result.push(`${value} * ${i+2} = ${result}`);
+      value = result;
+    }
+
+    for (let i = 0; i <= this.height; i++) {
+      const result = value / (i+2);
+      this.result.push(`${value} / ${i+2} = ${result}`);
+      value = result;
+    }
+
+  }
 }
